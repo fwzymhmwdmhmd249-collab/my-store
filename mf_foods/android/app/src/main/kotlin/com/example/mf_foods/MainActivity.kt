246f19b2e40a8cc3dfca139aebc2dac5f7032efa
fwzymhmwdmhmd249-collab <fwzymhmwdmhmd249@gmail.com>
2026-08-05 18:15:24 +0000

    Fix webview config and build debug apk

--- mf_foods/android/app/src/main/kotlin/com/example/mf_foods/MainActivity.kt
+++ mf_foods/android/app/src/main/kotlin/com/example/mf_foods/MainActivity.kt
@@ -3,3 +3,7 @@ package com.example.mf_foods
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+WebSettings webSettings = webView.getSettings();
+webSettings.setJavaScriptEnabled(true);
+webSettings.setDomStorageEnabled(true);
+webSettings.setDatabaseEnabled(true);
