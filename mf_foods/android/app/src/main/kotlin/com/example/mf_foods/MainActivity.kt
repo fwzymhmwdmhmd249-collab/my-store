94e74752b311c289bfca90803cc0e3a0cc265b5e
fwzymhmwdmhmd249-collab <fwzymhmwdmhmd249@gmail.com>
2026-08-06 15:42:38 +0000

    Fix MainActivity and update workflow

--- mf_foods/android/app/src/main/kotlin/com/example/mf_foods/MainActivity.kt
+++ mf_foods/android/app/src/main/kotlin/com/example/mf_foods/MainActivity.kt
@@ -2,8 +2,4 @@ package com.example.mf_foods
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
-WebSettings webSettings = webView.getSettings();
-webSettings.setJavaScriptEnabled(true);
-webSettings.setDomStorageEnabled(true);
-webSettings.setDatabaseEnabled(true);
+class MainActivity: FlutterActivity()
